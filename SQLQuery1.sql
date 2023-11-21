6e7cd5b462fcae4e8fe58ceecf3608cc0e7f5465
Manuel Marin <cmosea.1202@hotmail.com>
2023-11-20 21:02:34 -0500

    red

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,10 @@
 SELECT* FROM Usuario
 UPDATE Usuario SET Codigo='R1111',Nombre='Juan',ApellidoPaterno='Perez',ApellidoMaterno='Quispe',Dni='71426464',NombreFoto='Cejo.jpg',Contraseña='esotilin' WHERE Codigo='R1111'
 SELECT* FROM PedidoCambio
+
+SELECT*FROM Emergencia where Fecha='19/11/2023'
+SELECT*FROM Emergencia where Fecha like '%19%'
+UPDATE Emergencia set Fecha='12/11/2023' where Fecha like '%19%'
+
+select*from Emergencia where Fecha >= '12/11/2023'
+  and Fecha < '17/11/2023'
